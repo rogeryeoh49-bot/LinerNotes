e6e6cabf0bc4c283a54bf3aeb3387a20868f901b
Roger Yeoh <rogeryeoh49@gmail.com>
2026-06-18 21:12:46 +0800

    Initial Commit

---
+++ LinerNotes/LinerNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinerNotesApp.swift
+//  LinerNotes
+//
+//  Created by Roger Yeoh on 6/18/26.
+//
+
+import SwiftUI
+
+@main
+struct LinerNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
